5c55057613331ef5b6fd0e3626b4cde8d035492a
Zet5a <40255270+zetaontherocks@users.noreply.github.com>
2023-10-21 21:19:23 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-let saveEl = document.getElementById("save-el");
-let countEl = document.getElementById("count-el");
-let count = 0;
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+let count = 0
 
 function increment() {
-    count += 1;
-    countEl.textContent = count;
+    count += 1
+    countEl.textContent = count
 }
 
 function save() {
-    let countStr = count + " - ";
-    saveEl.textContent += countStr;
-    countEl.textContent = 0;
-    count = 0;
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+    countEl.textContent = 0
+    count = 0
 }
 
 function reset(){
-    saveEl.textContent = "Previous entries: ";
-    countEl.textContent = 0;
+    saveEl.textContent = "Previous entries: "
+    countEl.textContent = 0
 }
